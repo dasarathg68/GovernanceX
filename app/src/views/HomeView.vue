809ad1f730f69e5b05d3606b2c19b09162658268
Dasarath G <dasarathg68@gmail.com>
2024-08-05 23:32:07 +0530

    init

---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  Hi
+  <div class="text-2xl bg-stone-800">Home</div>
+  <p>Click on the links above to navigate to different pages.</p>
+</template>
